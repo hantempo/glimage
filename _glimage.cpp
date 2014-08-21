ebbf904e7ea12d81caf91a635460dd89278d3ad1
hantempo <hantempo@gmail.com>
2014-08-21 21:33:33 +0200

    Revert "Revert "Add from GL_R8, GL_RGBA5551, GL_LUMINANCE, GL_ALPHA, GL_LUMINANCE_ALPHA to GL_RGBA8""
    This reverts commit 04399dac8c466b7e32dbc06506ea473236822cbc.

--- _glimage.cpp
+++ _glimage.cpp
@@ -5,15 +5,22 @@
 static char module_docstring[] =
     "This module provides some methods to convert pixels of images between different OpenGL formats.";
 
+static PyObject *glimage_r8_to_rgba8(PyObject *self, PyObject *args);
+static PyObject *glimage_rgb565_to_rgba8(PyObject *self, PyObject *args);
 static PyObject *glimage_rgba4_to_rgba8(PyObject *self, PyObject *args);
-static PyObject *glimage_rgb565_to_rgb8(PyObject *self, PyObject *args);
+static PyObject *glimage_rgba5551_to_rgba8(PyObject *self, PyObject *args);
+static PyObject *glimage_luminance_alpha_to_rgba8(PyObject *self, PyObject *args);
+static PyObject *glimage_alpha_to_rgba8(PyObject *self, PyObject *args);
+static PyObject *glimage_luminance_to_rgba8(PyObject *self, PyObject *args);
 
 static PyMethodDef module_methods[] = {
-    // RGB
-    {"rgb565_to_rgb8", glimage_rgb565_to_rgb8, METH_VARARGS, "Convert pixels from GL_RGB565 to GL_RGB8"},
-
-    // RGBA
+    {"r8_to_rgba8", glimage_r8_to_rgba8, METH_VARARGS, "Convert pixels from GL_R8 to GL_RGBA8"},
+    {"rgb565_to_rgba8", glimage_rgb565_to_rgba8, METH_VARARGS, "Convert pixels from GL_RGB565 to GL_RGBA8"},
     {"rgba4_to_rgba8", glimage_rgba4_to_rgba8, METH_VARARGS, "Convert pixels from GL_RGBA4 to GL_RGBA8"},
+    {"rgba5551_to_rgba8", glimage_rgba5551_to_rgba8, METH_VARARGS, "Convert pixels from GL_RGBA5551 to GL_RGBA8"},
+    {"luminance_alpha_to_rgba8", glimage_luminance_alpha_to_rgba8, METH_VARARGS, "Convert pixels from GL_LUMINANCE_ALPHA to GL_RGBA8"},
+    {"alpha_to_rgba8", glimage_alpha_to_rgba8, METH_VARARGS, "Convert pixels from GL_ALPHA to GL_RGBA8"},
+    {"luminance_to_rgba8", glimage_luminance_to_rgba8, METH_VARARGS, "Convert pixels from GL_LUMINANCE to GL_RGBA8"},
 
     {NULL, NULL, 0, NULL}
 };
@@ -28,7 +35,7 @@ PyMODINIT_FUNC init_glimage(void)
     import_array();
 }
 
-#define DEFINE_CONVERSION_WRAPPER(func_name, input_element_type, output_element_type) \
+#define DEFINE_CONVERSION_WRAPPER(func_name, input_numpy_type, input_c_type, output_numpy_type, output_c_type) \
 static PyObject *glimage_##func_name(PyObject *self, PyObject *args) \
 { \
     unsigned int width, height; \
@@ -40,8 +47,8 @@ static PyObject *glimage_##func_name(PyObject *self, PyObject *args) \
         return NULL; \
      \
     /* Interpret the input objects as numpy arrays. */ \
-    PyObject *input_pixels_array = PyArray_FROM_OTF(input_pixels_obj, input_element_type, NPY_IN_ARRAY); \
-    PyObject *output_pixels_array = PyArray_FROM_OTF(output_pixels_obj, output_element_type, NPY_INOUT_ARRAY); \
+    PyObject *input_pixels_array = PyArray_FROM_OTF(input_pixels_obj, input_numpy_type, NPY_IN_ARRAY); \
+    PyObject *output_pixels_array = PyArray_FROM_OTF(output_pixels_obj, output_numpy_type, NPY_INOUT_ARRAY); \
      \
     /* If that didn't work, throw an exception. */ \
     if (input_pixels_array == NULL || output_pixels_array == NULL) { \
@@ -51,8 +58,8 @@ static PyObject *glimage_##func_name(PyObject *self, PyObject *args) \
     } \
      \
     /* Get pointers to the data as C-types. */ \
-    unsigned short *input_pixels = (unsigned short*)PyArray_DATA(input_pixels_array); \
-    unsigned char *output_pixels = (unsigned char*)PyArray_DATA(output_pixels_array); \
+    input_c_type *input_pixels = (input_c_type*)PyArray_DATA(input_pixels_array); \
+    output_c_type *output_pixels = (output_c_type*)PyArray_DATA(output_pixels_array); \
      \
     const bool status = func_name(width, height, input_pixels, output_pixels); \
      \
@@ -64,5 +71,10 @@ static PyObject *glimage_##func_name(PyObject *self, PyObject *args) \
     return Py_BuildValue("O", status ? Py_True : Py_False); \
 }
 
-DEFINE_CONVERSION_WRAPPER(rgba4_to_rgba8, NPY_UINT16, NPY_UINT8)
-DEFINE_CONVERSION_WRAPPER(rgb565_to_rgb8, NPY_UINT16, NPY_UINT8)
+DEFINE_CONVERSION_WRAPPER(r8_to_rgba8, NPY_UINT8, unsigned char, NPY_UINT8, unsigned char)
+DEFINE_CONVERSION_WRAPPER(rgb565_to_rgba8, NPY_UINT16, unsigned short, NPY_UINT8, unsigned char)
+DEFINE_CONVERSION_WRAPPER(rgba4_to_rgba8, NPY_UINT16, unsigned short, NPY_UINT8, unsigned char)
+DEFINE_CONVERSION_WRAPPER(rgba5551_to_rgba8, NPY_UINT16, unsigned short, NPY_UINT8, unsigned char)
+DEFINE_CONVERSION_WRAPPER(luminance_alpha_to_rgba8, NPY_UINT8, unsigned char, NPY_UINT8, unsigned char)
+DEFINE_CONVERSION_WRAPPER(luminance_to_rgba8, NPY_UINT8, unsigned char, NPY_UINT8, unsigned char)
+DEFINE_CONVERSION_WRAPPER(alpha_to_rgba8, NPY_UINT8, unsigned char, NPY_UINT8, unsigned char)
--- glimage.h
+++ glimage.h
@@ -1,5 +1,20 @@
-bool rgb565_to_rgb8(unsigned int width, unsigned int height,
+bool r8_to_rgba8(unsigned int width, unsigned int height,
+    unsigned char *input_pixels, unsigned char *output_pixels);
+
+bool rgb565_to_rgba8(unsigned int width, unsigned int height,
     unsigned short *input_pixels, unsigned char *output_pixels);
 
 bool rgba4_to_rgba8(unsigned int width, unsigned int height,
     unsigned short *input_pixels, unsigned char *output_pixels);
+
+bool rgba5551_to_rgba8(unsigned int width, unsigned int height,
+    unsigned short *input_pixels, unsigned char *output_pixels);
+
+bool luminance_alpha_to_rgba8(unsigned int width, unsigned int height,
+    unsigned char *input_pixels, unsigned char *output_pixels);
+
+bool alpha_to_rgba8(unsigned int width, unsigned int height,
+    unsigned char *input_pixels, unsigned char *output_pixels);
+
+bool luminance_to_rgba8(unsigned int width, unsigned int height,
+    unsigned char *input_pixels, unsigned char *output_pixels);
